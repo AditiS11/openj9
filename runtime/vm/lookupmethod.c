4dfe4bff5124c2075032fee14cc2b0d4df52a3d7
Aditi Srinivas <Aditi.Srini@ibm.com>
2025-08-27 22:21:18 -0700

    fixup

--- runtime/vm/lookupmethod.c
+++ runtime/vm/lookupmethod.c
@@ -701,6 +701,7 @@ retry:
 			if (foundMethod != NULL) {
 				UDATA modifiers = J9_ROM_METHOD_FROM_RAM_METHOD(foundMethod)->modifiers;
 				if (J9_ARE_ANY_BITS_SET(lookupOptions, J9_INVOKEINTERFACE) && J9_ARE_ANY_BITS_SET(modifiers, J9AccPrivate)) {
+					foundMethod = NULL;
 					goto nextClass;
 				}
 				resultMethod = processMethod(currentThread, lookupOptions, foundMethod, lookupClass, &exception, &exceptionClass, &errorType, nameAndSig, senderClass, targetClass);
